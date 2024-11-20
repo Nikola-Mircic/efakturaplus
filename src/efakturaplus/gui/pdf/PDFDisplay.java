275d9a69b6c28e27f2d744d77cc272076b5eeb1b
Nikola-Mircic <nikola123mircic@gmail.com>
2024-11-20 21:53:13 +0100

    Displaying PDF pages verticaly

--- src/efakturaplus/gui/pdf/PDFDisplay.java
+++ src/efakturaplus/gui/pdf/PDFDisplay.java
@@ -1,6 +1,5 @@
 package efakturaplus.gui.pdf;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
 import java.awt.*;
@@ -8,11 +7,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.Serial;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class PDFDisplay extends JPanel implements KeyListener {
+	@Serial
 	private static final long serialVersionUID = 1L;
 	// byte array containing the PDF file content
 	private int pageIndex = 0;
@@ -21,19 +22,14 @@ public class PDFDisplay extends JPanel implements KeyListener {
 	private int y_offset = 0;
 
 	private ArrayList<BufferedImage> pages;
-	private CardLayout pagesLayout;
 	private JPanel pagesPanel;
 
-	private PDDocument pdfDocument;
-	private PDFRenderer renderer;
+	private final PDDocument pdfDocument;
+	private final PDFRenderer renderer;
 
 	public PDFDisplay(PDDocument pdfDocument) throws IOException {
 		this.pdfDocument = pdfDocument;
 		this.renderer = new PDFRenderer(pdfDocument);
-
-		pagesLayout = new CardLayout();
-		pagesPanel = new JPanel(pagesLayout);
-		this.add(pagesPanel);
 
 		drawPages();
 		updateLayout();
@@ -46,28 +42,33 @@ public class PDFDisplay extends JPanel implements KeyListener {
 		}
 	}
 
-	private void updateLayout() throws IOException {
-		pagesPanel.removeAll();
+	private void updateLayout(){
+		this.removeAll();
 
-		for(int i = 0; i<pdfDocument.getNumberOfPages(); ++i){
-			final int idx = i;
-			JPanel imagePanel = new JPanel(){
-				@Override
-				public void paint(Graphics g) {
-					g.drawImage(pages.get(idx), x_offset, y_offset,  null);
-					g.dispose();
+		this.pagesPanel = new JPanel(){
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+
+				int pageHeight = pages.getFirst().getHeight() + 5;
+
+				System.out.println("Page height: " + pageHeight);
+				System.out.println("[x_offest, y_offset]: " + x_offset + "," + y_offset);
+
+				for(int i = 0; i<pages.size(); ++i) {
+					g.drawImage(pages.get(i), x_offset, (i - pageIndex) * pageHeight + y_offset, null);
 				}
 
-				@Override
-				public Dimension getPreferredSize() {
-					return new Dimension(pages.get(idx).getWidth(), pages.get(idx).getHeight());
-				}
-			};
+				g.dispose();
+			}
 
-			pagesPanel.add(imagePanel, String.valueOf(i));
-		}
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(pages.getFirst().getWidth(), pages.getFirst().getHeight());
+			}
+		};
 
-		pagesLayout.show(pagesPanel, String.valueOf(pageIndex));
+		this.add(pagesPanel);
 	}
 
 	@Override
@@ -81,7 +82,7 @@ public class PDFDisplay extends JPanel implements KeyListener {
 			// Zooming and moving displayed PDF file
 			switch (e.getKeyCode()){
 				case KeyEvent.VK_ADD -> {
-					this.scale += 0.05;
+					this.scale += 0.05F;
                     try {
                         drawPages();
                     } catch (IOException ex) {
@@ -89,7 +90,7 @@ public class PDFDisplay extends JPanel implements KeyListener {
                     }
                 }
 				case KeyEvent.VK_SUBTRACT -> {
-					this.scale -= 0.05;
+					this.scale -= 0.05F;
 					try {
 						drawPages();
 					} catch (IOException ex) {
@@ -102,18 +103,6 @@ public class PDFDisplay extends JPanel implements KeyListener {
 				case KeyEvent.VK_RIGHT -> {
 					x_offset += 20;
 				}
-				case KeyEvent.VK_UP -> {
-					y_offset -= 20;
-				}
-				case KeyEvent.VK_DOWN -> {
-					y_offset += 20;
-				}
-			}
-
-			try {
-				this.updateLayout();
-			} catch (IOException ex) {
-				throw new RuntimeException(ex);
 			}
 		}else{
 			// Switching between pages
@@ -122,20 +111,29 @@ public class PDFDisplay extends JPanel implements KeyListener {
 					this.pageIndex++;
 
 					this.pageIndex = Math.min(this.pageIndex, this.pdfDocument.getNumberOfPages() - 1);
+					y_offset = 0;
 
-					pagesLayout.show(pagesPanel, String.valueOf(pageIndex));
 					System.out.println(pageIndex);
 				}
 				case KeyEvent.VK_LEFT -> {
 					this.pageIndex--;
 
 					this.pageIndex = Math.max(this.pageIndex, 0);
+					y_offset = 0;
 
-					pagesLayout.show(pagesPanel, String.valueOf(pageIndex));
 					System.out.println(pageIndex);
+				}
+				case KeyEvent.VK_UP -> {
+					y_offset += 20;
+				}
+				case KeyEvent.VK_DOWN -> {
+					y_offset -= 20;
 				}
 			}
 		}
+
+		this.pagesPanel.repaint();
+		this.repaint();
     }
 
 	@Override
